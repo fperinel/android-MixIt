4c00c9e4ba51ce758b8fba5486c7aadc65bd3759
Florent PERINEL <florent.perinel@gmail.com>
2013-04-13 20:27:16 +0100

    Fixes #8 Adresse de l'événement (googlemap et navigation)

--- src/main/java/com/level42/mixit/activities/AdresseActivity.java
+++ src/main/java/com/level42/mixit/activities/AdresseActivity.java
@@ -2,6 +2,11 @@ package com.level42.mixit.activities;
 
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
+import roboguice.inject.InjectResource;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
 
 import com.level42.mixit.R;
 
@@ -11,4 +16,26 @@ import com.level42.mixit.R;
 @ContentView(R.layout.activity_adresse)
 public class AdresseActivity extends RoboActivity {
 
+    @InjectResource(R.string.mixit_address)
+    private String adresse;
+    
+    /**
+     * Ouvre la navigation
+     * @param v
+     */
+    public void actionGoToAddress(View v) {
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("google.navigation:q=" + adresse));
+        startActivity(intent);
+    }
+    
+    /**
+     * Ouvre la google map
+     * @param v
+     */
+    public void actionShowAddress(View v) {
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("http://maps.google.com/maps?q=" + adresse));
+        startActivity(intent);
+    }
 }
